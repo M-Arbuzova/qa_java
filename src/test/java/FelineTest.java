fc97be4524703de4bd6ea431b9d469f330f69a14
Mariia Arbuzova <caprizzzzz@yandex.ru>
2023-03-27 15:26:47 +0300

    Changed class Lion, add Test, changed pom.xml and gitignore

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,39 @@
-import static org.junit.Assert.*;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
-  
+    private final int expectedKittenCount = 1;
+    private final List<String> expectedFoodFeline = List.of("Животные", "Птицы", "Рыба");
+
+    @Test
+    public void eatMeatFelineTest() throws Exception {
+        Feline feline = new Feline();
+        assertEquals(expectedFoodFeline, feline.eatMeat());
+    }
+
+    @Test
+    public void getFamilyFelineTest() {
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensFelineTest() {
+        Feline feline = new Feline();
+        assertEquals(expectedKittenCount, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensFelineTest2() {
+        Feline feline = new Feline();
+        assertEquals(5, feline.getKittens(5));
+
+    }
 }
--- src/test/java/LionParamTest.java
+++ src/test/java/LionParamTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class LionParamTest {
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionParamTest {
+    private final boolean expectedResultMane;
+    Feline feline;
+    private final String sex;
+
+    public LionParamTest(String sex, boolean expectedResultMane) {
+        this.sex = sex;
+        this.expectedResultMane = expectedResultMane;
+    }
+
+    @Parameters(name = "{index}: Тестовые данные: пол {0}, наличие гривы {1}")
+    public static Object[][] checkHaveMane() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+
+        };
+    }
+
+    @Test
+    public void doesManeTest() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        assertEquals(expectedResultMane, lion.doesHaveMane());
+    }
 }
